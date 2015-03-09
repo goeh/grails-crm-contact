32038fb40cdb8cd45b9ac46e78b2d5850906dc74
Goran Ehrsson <goran@technipelago.se>
2015-03-09 22:58:02 +0100

    Disabled forked mode for tests because Cobertura complains

--- grails-app/conf/BuildConfig.groovy
+++ grails-app/conf/BuildConfig.groovy
@@ -3,6 +3,14 @@ grails.project.test.class.dir = "target/test-classes"
 grails.project.test.reports.dir = "target/test-reports"
 grails.project.target.level = 1.6
 
+grails.project.fork = [
+    //  compile: [maxMemory: 256, minMemory: 64, debug: false, maxPerm: 256, daemon:true],
+    test: false,
+    run: [maxMemory: 768, minMemory: 64, debug: false, maxPerm: 256, forkReserve:false],
+    war: [maxMemory: 768, minMemory: 64, debug: false, maxPerm: 256, forkReserve:false],
+    console: [maxMemory: 768, minMemory: 64, debug: false, maxPerm: 256]
+]
+
 grails.project.dependency.resolver = "maven"
 grails.project.dependency.resolution = {
     inherits "global"
