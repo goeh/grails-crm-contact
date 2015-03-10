16d0eb3acb44917b0519e5441327257863628921
Goran Ehrsson <goran@technipelago.se>
2015-03-10 10:01:58 +0100

    Version 2.4.0

--- CrmContactGrailsPlugin.groovy
+++ CrmContactGrailsPlugin.groovy
@@ -18,7 +18,7 @@ import grails.plugins.crm.contact.CrmContact
 
 class CrmContactGrailsPlugin {
     def groupId = ""
-    def version = "2.4.0-SNAPSHOT"
+    def version = "2.4.0"
     def grailsVersion = "2.2 > *"
     def dependsOn = [:]
     def loadAfter = ['crmCore', 'selection', 'crmTags']
--- grails-app/conf/BuildConfig.groovy
+++ grails-app/conf/BuildConfig.groovy
@@ -39,8 +39,8 @@ grails.project.dependency.resolution = {
         test(":codenarc:0.22") { export = false }
         test(":code-coverage:2.0.3-3") { export = false }
 
-        compile ":crm-security:2.4.0-SNAPSHOT"
-        compile ":crm-tags:2.4.0-SNAPSHOT"
+        compile ":crm-security:2.4.0"
+        compile ":crm-tags:2.4.0"
 
         compile ":sequence-generator:1.1"
         compile ":selection:0.9.8"
