eed082db4f2683b845ec72b7e1edf3661ce602bd
Goran Ehrsson <goran@technipelago.se>
2016-01-17 19:23:53 +0100

    Next SNAPSHOT version

--- CrmContactGrailsPlugin.groovy
+++ CrmContactGrailsPlugin.groovy
@@ -18,7 +18,7 @@ import grails.plugins.crm.contact.CrmContact
 
 class CrmContactGrailsPlugin {
     def groupId = ""
-    def version = "2.4.1"
+    def version = "2.4.2-SNAPSHOT"
     def grailsVersion = "2.2 > *"
     def dependsOn = [:]
     def loadAfter = ['crmCore', 'selection', 'crmTags']
